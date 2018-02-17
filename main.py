08dca66f4e17783ab9cbc174f9ef9884f4d1717f
Michael Danko <michael@danko.email>
2018-02-17 21:27:35 +0000

    added findContractCalls function

--- main.py
+++ main.py
@@ -14,22 +14,35 @@ rpc = ParityEthJsonRpc('127.0.0.1', 8545)
 
 def findContractCreationBlock(contract):
   latestBlock = rpc.eth_blockNumber()
-  for i in range(latestBlock, 5934492, -1):
-    print("Searching:" + str(i))
+
+  # shortcut since we already know block creation date
+  return(5934492)
+
+  for i in range(latestBlock, 0, -1):
     data = rpc.eth_getBlockByNumber(i, True)
     for j in range(0, len(data["transactions"]), 1):
       if data["transactions"][j]["creates"] == contract:
-        print(int(data["number"], 16))
-  return(data)
+        return(int(data["number"], 16))
+
+def findContractCalls(startBlock, contract):
+  latestBlock = rpc.eth_blockNumber()
+  for i in range(startBlock, latestBlock, 1):
+    data = rpc.eth_getBlockByNumber(i, True)
+    for j in range(0, len(data["transactions"]), 1):
+      if data["transactions"][j]["to"] == contract:
+        print(json.dumps(data, indent=3))
+
 
 
 def main():
 #  print(json.dumps(rpc.eth_getBlockByNumber(5934492, True), indent=4))
-  createdBlock = 5934492
-  transactionBlock = 5934537
-  findContractCreationBlock('0x719ea19781af64a5f3997fc2b93368d1fe643066')
+#  createdBlock = 5934492
+#  transactionBlock = 5934537
+  contractToTrack = '0x719ea19781af64a5f3997fc2b93368d1fe643066'
+  creationBlock = findContractCreationBlock(contractToTrack)
+  findContractCalls(creationBlock, contractToTrack) 
 #  print(foundContract)
-  data = rpc.eth_getBlockByNumber(transactionBlock, True)
+#  data = rpc.eth_getBlockByNumber(transactionBlock, True)
 #  for i in range(latestBlock, latestBlock - 1000, -1):
 #    data = rpc.eth_getBlockByNumber(i, True)
 #    
