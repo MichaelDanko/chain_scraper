663509170e7d270ebd16637a67d8ef2b1559590e
Michael Danko <michael@danko.email>
2018-02-17 22:32:48 +0000

    added code to output to CSV

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ __license__ = "MIT"
 
 import json
 from ethjsonrpc import ParityEthJsonRpc
+import csv
 
 rpc = ParityEthJsonRpc('127.0.0.1', 8545)
 
@@ -13,10 +14,11 @@ rpc = ParityEthJsonRpc('127.0.0.1', 8545)
 # 0x719ea19781af64a5f3997fc2b93368d1fe643066
 
 def findContractCreationBlock(contract):
-  latestBlock = rpc.eth_blockNumber()
 
   # shortcut since we already know block creation date
   return(5934492)
+
+  latestBlock = rpc.eth_blockNumber()
 
   for i in range(latestBlock, 0, -1):
     data = rpc.eth_getBlockByNumber(i, True)
@@ -24,32 +26,37 @@ def findContractCreationBlock(contract):
       if data["transactions"][j]["creates"] == contract:
         return(int(data["number"], 16))
 
-def findContractCalls(startBlock, contract):
-  latestBlock = rpc.eth_blockNumber()
-  for i in range(startBlock, latestBlock, 1):
-    data = rpc.eth_getBlockByNumber(i, True)
-    for j in range(0, len(data["transactions"]), 1):
-      if data["transactions"][j]["to"] == contract:
-        print(json.dumps(data, indent=3))
+#-----------------------------------
 
+def findContractCalls(startBlock, contract):
+  with open('chain_data.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["timestamp", "Block Number", "Transaction Hash", "Transaction Index", "From", "Value", "Gas Price", "Gas Used", "Function Called"])
+    latestBlock = rpc.eth_blockNumber()
+    for i in range(startBlock, latestBlock, 1):
+      data = rpc.eth_getBlockByNumber(i, True)
+      for j in range(0, len(data["transactions"]), 1):
+        if data["transactions"][j]["to"] == contract:
+          receipt = rpc.eth_getTransactionReceipt(data["transactions"][j]['hash'])
+          gasPrice = int(data["transactions"][j]["gasPrice"], 16)
+          gasUsed = int(receipt['gasUsed'], 16) 
+
+          writer.writerow([int(data["timestamp"], 16),
+          int(data['number'], 16),
+          data['transactions'][j]['hash'],
+          int(data['transactions'][j]['transactionIndex'], 16),
+          data["transactions"][j]["from"],
+          int(data["transactions"][j]["value"], 16),
+          gasPrice, 
+          gasUsed,
+          (gasPrice * gasUsed),
+          data['transactions'][j]['raw'][:6]])
 
 
 def main():
-#  print(json.dumps(rpc.eth_getBlockByNumber(5934492, True), indent=4))
-#  createdBlock = 5934492
-#  transactionBlock = 5934537
   contractToTrack = '0x719ea19781af64a5f3997fc2b93368d1fe643066'
   creationBlock = findContractCreationBlock(contractToTrack)
   findContractCalls(creationBlock, contractToTrack) 
-#  print(foundContract)
-#  data = rpc.eth_getBlockByNumber(transactionBlock, True)
-#  for i in range(latestBlock, latestBlock - 1000, -1):
-#    data = rpc.eth_getBlockByNumber(i, True)
-#    
-#  for j in range(0, len(data["transactions"]), 1):
-#    #if data["transactions"][j]["creates"]:
-#    print(json.dumps(data["transactions"], indent=2))
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
     main()
